b3a64a096ce85e257b29e7a52f0538c39d6ba6ee
Claas <claas.moehlmann@outlook.com>
2024-01-05 19:24:31 +0100

    Remove unnecessary stuff

--- theme/index.php
+++ theme/index.php
@@ -15,39 +15,36 @@
 get_header();
 ?>
 
-	<section id="primary">
-		<main id="main">
 
-		<?php
-		if ( have_posts() ) {
+<?php
+if (have_posts()) {
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header class="entry-header">
-					<h1 class="entry-title"><?php single_post_title(); ?></h1>
-				</header><!-- .entry-header -->
-				<?php
-			endif;
-
-			// Load posts loop.
-			while ( have_posts() ) {
-				the_post();
-				get_template_part( 'template-parts/content/content' );
-			}
-
-			// Previous/next page navigation.
-			claas_dev_the_posts_navigation();
-
-		} else {
-
-			// If no content, include the "No posts found" template.
-			get_template_part( 'template-parts/content/content', 'none' );
-
-		}
+	if (is_home() && !is_front_page()):
 		?>
+		<header class="entry-header">
+			<h1 class="entry-title">
+				<?php single_post_title(); ?>
+			</h1>
+		</header><!-- .entry-header -->
+		<?php
+	endif;
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+	// Load posts loop.
+	while (have_posts()) {
+		the_post();
+		get_template_part('template-parts/content/content');
+	}
+
+	// Previous/next page navigation.
+	claas_dev_the_posts_navigation();
+
+} else {
+
+	// If no content, include the "No posts found" template.
+	get_template_part('template-parts/content/content', 'none');
+
+}
+?>
 
 <?php
 get_footer();
--- theme/template-parts/content/content.php
+++ theme/template-parts/content/content.php
@@ -9,35 +9,37 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('max-w-[50rem] bg-slate-100 p-4 rounded-3xl'); ?>>
 
 	<header class="entry-header">
 		<?php
-		if ( is_sticky() && is_home() && ! is_paged() ) {
-			printf( '<span">%s</span>', esc_html_x( 'Featured', 'post', 'claas-dev' ) );
+
+		$title_class = 'text-4xl';
+		if (is_sticky() && is_home() && !is_paged()) {
+			printf('<span">%s</span>', esc_html_x('Featured', 'post', 'claas-dev'));
 		}
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		if (is_singular()):
+			the_title('<h1 class="' . $title_class . '">', '</h1>');
+		else:
+			the_title(sprintf('<h1 class="' . $title_class . '"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>');
 		endif;
 		?>
 	</header><!-- .entry-header -->
 
 	<?php claas_dev_post_thumbnail(); ?>
 
-	<div <?php claas_dev_content_class( 'entry-content' ); ?>>
+	<section <?php claas_dev_content_class('entry-content'); ?>>
 		<?php
 		the_content();
 
 		wp_link_pages(
 			array(
-				'before' => '<div>' . __( 'Pages:', 'claas-dev' ),
-				'after'  => '</div>',
+				'before' => '<div>' . __('Pages:', 'claas-dev'),
+				'after' => '</div>',
 			)
 		);
 		?>
-	</div><!-- .entry-content -->
+	</section><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php claas_dev_entry_footer(); ?>
