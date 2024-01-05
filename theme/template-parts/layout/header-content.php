a75b75847697296db0f5cc22a8c5ff05f2ad5c8b
Claas <claas.moehlmann@outlook.com>
2024-01-05 18:54:59 +0100

    Style title

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -11,22 +11,25 @@
 
 <header id="masthead">
 
-	<?php
-	if ( is_front_page() ) :
-		?>
-		<h1><?php bloginfo( 'name' ); ?></h1>
-		<?php
-	else :
-		?>
-		<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-		<?php
-	endif;
 
-	$claas_dev_description = get_bloginfo( 'description', 'display' );
-	if ( $claas_dev_description || is_customize_preview() ) :
-		?>
-		<p><?php echo $claas_dev_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-	<?php endif; ?>
+	<hgroup>
+		<?php
+		if ( is_front_page() ) :
+			?>
+			<h1 class="text-9xl font-light lowercase"><?php bloginfo( 'name' ); ?></h1>
+			<?php
+		else :
+			?>
+			<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+		endif;
+
+		$claas_dev_description = get_bloginfo( 'description', 'display' );
+		if ( $claas_dev_description || is_customize_preview() ) :
+			?>
+			<p class="text-4xl"><?php echo $claas_dev_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+		<?php endif; ?>
+	</hgroup>
 
 
 </header><!-- #masthead -->
