49659bc69ca5d0a4398d9e08f9b0800d1d3a9552
Claas <claas.moehlmann@outlook.com>
2024-01-07 18:27:40 +0100

    Improve visual hierarchy between posts and header

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -9,25 +9,31 @@
 
 ?>
 
-<header id="masthead">
+<header id="masthead" class="mb-6">
 
 
 	<hgroup>
 		<?php
-		if ( is_front_page() ) :
+		if (is_front_page()):
 			?>
-			<h1 class="text-9xl font-light lowercase"><?php bloginfo( 'name' ); ?></h1>
+			<h1 class="text-9xl font-light lowercase">
+				<?php bloginfo('name'); ?>
+			</h1>
 			<?php
-		else :
+		else:
 			?>
-			<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<p><a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+					<?php bloginfo('name'); ?>
+				</a></p>
 			<?php
 		endif;
 
-		$claas_dev_description = get_bloginfo( 'description', 'display' );
-		if ( $claas_dev_description || is_customize_preview() ) :
+		$claas_dev_description = get_bloginfo('description', 'display');
+		if ($claas_dev_description || is_customize_preview()):
 			?>
-			<p class="text-4xl"><?php echo $claas_dev_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<p class="text-4xl">
+				<?php echo $claas_dev_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+			</p>
 		<?php endif; ?>
 	</hgroup>
 
