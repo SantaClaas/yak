0daa198297999459cd1e1fdab4b9daafad94f7c3
Claas <claas.moehlmann@outlook.com>
2024-01-07 18:28:08 +0100

    Improve image and add author

--- theme/template-parts/content/content.php
+++ theme/template-parts/content/content.php
@@ -9,41 +9,30 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('max-w-[50rem] bg-slate-100 p-4 rounded-3xl'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('max-w-[40rem] bg-slate-100 p-4 rounded-xl p-4 space-y-3'); ?>>
 
-	<header class="entry-header">
+	<?php the_post_thumbnail('post-thumbnail', ['class' => 'rounded-xl h-80 object-cover object-center']); ?>
+
+	<section class="text-xl">
 		<?php
-
-		$title_class = 'text-4xl';
-		if (is_sticky() && is_home() && !is_paged()) {
-			printf('<span">%s</span>', esc_html_x('Featured', 'post', 'claas-dev'));
-		}
-		if (is_singular()):
-			the_title('<h1 class="' . $title_class . '">', '</h1>');
-		else:
-			the_title(sprintf('<h1 class="' . $title_class . '"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>');
-		endif;
+		echo get_avatar(get_the_author_meta('ID'), $size = '60', $default = '', $alt = '', $args = array('class' => 'rounded-full h-7 w-7 inline-block align-middle'));
 		?>
-	</header><!-- .entry-header -->
+		<span class="align-middle">
+			<?php echo esc_html(get_the_author()) ?>
+		</span>
+	</section>
+	<?php
 
-	<?php claas_dev_post_thumbnail(); ?>
+	$title_class = 'text-3xl';
+	if (is_sticky() && is_home() && !is_paged()) {
+		printf('<span">%s</span>', esc_html_x('Featured', 'post', 'claas-dev'));
+	}
+	if (is_singular()):
+		the_title('<h1 class="' . $title_class . '">', '</h1>');
+	else:
+		the_title(sprintf('<h1 class="' . $title_class . '"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>');
+	endif;
+	?>
 
-	<!-- <section <?php claas_dev_content_class('entry-content'); ?>>
-		<?php
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before' => '<div>' . __('Pages:', 'claas-dev'),
-				'after' => '</div>',
-			)
-		);
-		?>
-	</section> -->
-	<!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php claas_dev_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 
 </article><!-- #post-${ID} -->
